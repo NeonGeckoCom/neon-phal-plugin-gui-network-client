81f8759823062495d6f70fddd9854901813da748
Daniel McKnight <daniel@neon.ai>
2022-09-08 09:21:59 -0700

    Fix copy/paste errors in scripts Add requirements.txt and version.py with updated references in setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,34 @@
 import os
 from setuptools import setup
 
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_version():
+    """ Find the version of the package"""
+    version = None
+    version_file = os.path.join(BASEDIR, 'ovos_PHAL_plugin_mk2', 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if alpha and int(alpha) > 0:
+        version += f"a{alpha}"
+    return version
+
+
 def package_files(directory):
     paths = []
     for (path, directories, filenames) in os.walk(directory):
@@ -9,10 +37,21 @@ def package_files(directory):
             paths.append(os.path.join('..', path, filename))
     return paths
 
+
+def required(requirements_file):
+    """ Read requirements file and remove comments and empty lines. """
+    with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
+        requirements = f.read().splitlines()
+        if 'MYCROFT_LOOSE_REQUIREMENTS' in os.environ:
+            print('USING LOOSE REQUIREMENTS!')
+            requirements = [r.replace('==', '>=').replace('~=', '>=') for r in requirements]
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
+
 PLUGIN_ENTRY_POINT = 'ovos-PHAL-plugin-gui-network-client=ovos_PHAL_plugin_gui_network_client:GuiNetworkClientPlugin'
 setup(
     name='ovos-PHAL-plugin-gui-network-client',
-    version='0.0.1',
+    version=get_version(),
     description='A plugin for OpenVoiceOS hardware abstraction layer',
     url='https://github.com/OpenVoiceOS/ovos-PHAL-plugin-gui-network-client',
     author='Aiix',
@@ -20,7 +59,7 @@ setup(
     license='Apache-2.0',
     packages=['ovos_PHAL_plugin_gui_network_client'],
     package_data={'': package_files('ovos_PHAL_plugin_gui_network_client')},
-    install_requires=["ovos-plugin-manager>=0.0.1"],
+    install_requires=required("requirements.txt"),
     zip_safe=True,
     include_package_data=True,
     classifiers=[
